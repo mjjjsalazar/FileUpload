01672fd109ca1640cfce88c7c648cd4b38a5ba8b
mjjjsalazar <mikhaeljastins@gmail.com>
2018-05-15 13:47:49 +0800

    FileUpload

--- application/views/users/add.php
+++ application/views/users/add.php
@@ -11,15 +11,47 @@
     .panel-transparent .panel-body{
     background: rgba(46, 51, 56, 0.2)!important;
 }
+.fontbg {
+    color:black;
+    padding:10px;
+    background-color:white;
+    opacity:0.7;
+    border:3px solid black;
+}
+.fonttitle {
+    color:black;
+    font-family: "Trebuchet MS";
+    font-weight:bold;
+}
+.fonttitle:hover {
+    transform: scale(1.2);
+    transition: 500ms;
+}
+.fontcolor {
+    color:black;
+    font-family: "Trebuchet MS";
+    padding:5px;
+    background-color:white;
+    border-radius:5px;
+    font-size:20px;
+    font-weight:bold;
+    border:3px solid black;
+}
+.fontcolor:hover {
+    padding:10px;
+    transform: scale(1.2);
+    transition: 500ms;
+}
 </style>
 
-<body background="bg.jpg">
+<body background="bg2.jpg">
 <div class="container">
-    <center><h1>File Upload </center></h1>
+    <center><h1 class="fontbg fonttitle">File Upload </center></h1>
 </div>
 <div class="container">
         <div class="row">
-            <div class="col-lg-12">
+        <div class="col-lg-4"></div>
+            <div class="col-lg-4">
                 <div class="row">
                     <br>
                     <div class="col-md-6"> <br><p  class="alert alert-success"> <?php echo $this->session->flashdata('success_msg'); ?></p> </div>
@@ -28,15 +60,15 @@
                             <div class="panel-transparent">
                                 <div class="panel-body">
                                     <div class="form-group">
-                                        <label>Picture</label>
+                                        <label class="fontcolor">Picture</label>
                                         <input class="form-control" type="file" name="picture" />
                                     </div>
                                     <div class="form-group">
-                                        <label>Name</label>
+                                        <label class="fontcolor">Name</label>
                                         <input class="form-control" type="text" name="name" />
                                     </div>
                                     <div class="form-group">
-                                        <label>Email</label>
+                                        <label class="fontcolor">Email</label>
                                         <input class="form-control" type="text" name="email" />
                                     </div>
                                     <br>
@@ -50,5 +82,6 @@
 </div>
 </div>
 </div>
+<div class="col-lg-4"></div>
 </body>
 </html>
